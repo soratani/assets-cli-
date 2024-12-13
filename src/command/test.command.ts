b000f7d5f58eb1414eccd64f3878ebe65f870941
liuwei <liuwei@zhihe.com>
2024-12-13 17:57:25 +0800

    wip: ui优化

--- src/command/test.command.ts
+++ src/command/test.command.ts
@@ -1,7 +1,8 @@
 import { Command } from "commander";
 import { AbstractCommand } from "./abstract.command";
-import { ApplicationConfig } from "@app/common/config/config";
+import { ApplicationConfig } from "@app/lib/config";
 import { get } from "lodash";
+import { Logger } from "@app/utils";
 
 export class TestCommand extends AbstractCommand {
     public load(program: Command): void {
@@ -14,8 +15,11 @@ export class TestCommand extends AbstractCommand {
                 const file = get(command, "config");
                 const env = get(command, "env");
                 const config = new ApplicationConfig(file, { env });
-                const instance = config.find('app', 'admin')
+                const instance = config.find('app', app);
+                if (!instance) Logger.error(`暂未找到当前应用${app}`);
                 instance.load();
+                console.log(instance);
+                // instance.load();
             })
 
     }
